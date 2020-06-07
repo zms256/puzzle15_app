f6f291a742cdf7f9ea23577067effd80e46ea526
zms256 <sks.aksk256@gmail.com>
2020-06-07 17:24:38 +0900

    ボタン番号を二次元配列化

--- lib/main.dart
+++ lib/main.dart
@@ -28,18 +28,73 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<int> _shuffle = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-  List<int> _topList = [1, 2, 3];
-  List<int> _cntList = [4, 5, 6];
-  List<int> _btmList = [0, 7, 8];
+  List<String> _shuffle = ['1', '2', '3', '4', '5', '6', '7', '8', ''];
+  List<List<String>> _numList = [
+    ['1', '2', '3'],
+    ['4', '5', '6'],
+    ['7', '8', '']
+  ];
+  bool _top;
+  bool _cnt;
+  bool _btm;
+  int _index;
 
-  void _gameStart(){
+  void _gameStart() {
     _shuffle.shuffle();
     setState(() {
-      _topList = [_shuffle[0], _shuffle[1], _shuffle[2]];
-      _cntList = [_shuffle[3], _shuffle[4], _shuffle[5]];
-      _btmList = [_shuffle[6], _shuffle[7], _shuffle[8]];
+      _numList = [
+        [_shuffle[0], _shuffle[1], _shuffle[2]],
+        [_shuffle[3], _shuffle[4], _shuffle[5]],
+        [_shuffle[6], _shuffle[7], _shuffle[8]]
+      ];
     });
+  }
+
+  void _changeNum(number) {
+    /*List<int> _sideNum = [];
+    List<String> _okList = [];
+    _top = _topList.contains('');
+    _cnt = _cntList.contains('');
+    _btm = _btmList.contains('');
+
+    _switchIndex(int index) {
+      switch (_index) {
+        case 0:
+          _sideNum.add(1);
+          break;
+        case 1:
+          _sideNum.add(0);
+          _sideNum.add(2);
+          break;
+        case 2:
+          _sideNum.add(1);
+          break;
+      }
+    }
+
+    if (_top == true) {
+      _index = _topList.indexOf('');
+      _switchIndex(_index);
+      _sideNum.forEach((element) {
+        _okList.add(_topList[element]);
+      });
+      _okList.add(_cntList[_index]);
+    } else if (_cnt == true) {
+      _index = _cntList.indexOf('');
+      _switchIndex(_index);
+      _sideNum.forEach((element) {
+        _okList.add(_cntList[element]);
+      });
+      _okList.add(_topList[_index]);
+      _okList.add(_btmList[_index]);
+    } else if (_btm == true) {
+      _index = _btmList.indexOf('');
+      _switchIndex(_index);
+      _sideNum.forEach((element) {
+        _okList.add(_btmList[element]);
+      });
+      _okList.add(_cntList[_index]);
+    }*/
   }
 
   @override
@@ -53,17 +108,16 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Padding(
-              padding: EdgeInsets.only(top: 50.0, bottom: 30.0),
-              child: FlatButton(
-                onPressed: (){
-                  _gameStart();
-                },
-                child: Text(
-                  'Tap here',
-                  style: TextStyle(fontSize: 30),
-                ),
-              )
-            ),
+                padding: EdgeInsets.only(top: 50.0, bottom: 30.0),
+                child: FlatButton(
+                  onPressed: () {
+                    _gameStart();
+                  },
+                  child: Text(
+                    'Tap here',
+                    style: TextStyle(fontSize: 30),
+                  ),
+                )),
             Expanded(
               child: Container(
                 child: Column(
@@ -76,9 +130,11 @@ class _MyHomePageState extends State<MyHomePage> {
                               width: double.infinity,
                               height: double.infinity,
                               child: FlatButton(
-                                onPressed: () {},
+                                onPressed: () {
+                                  _changeNum(_numList[0][0]);
+                                },
                                 child: Text(
-                                  _topList[0].toString(),
+                                  _numList[0][0],
                                   style: TextStyle(fontSize: 50),
                                 ),
                               ),
@@ -89,9 +145,11 @@ class _MyHomePageState extends State<MyHomePage> {
                               width: double.infinity,
                               height: double.infinity,
                               child: FlatButton(
-                                onPressed: () {},
+                                onPressed: () {
+                                  _changeNum(_numList[0][1]);
+                                },
                                 child: Text(
-                                  _topList[1].toString(),
+                                  _numList[0][1],
                                   style: TextStyle(fontSize: 50),
                                 ),
                               ),
@@ -102,54 +160,11 @@ class _MyHomePageState extends State<MyHomePage> {
                               width: double.infinity,
                               height: double.infinity,
                               child: FlatButton(
-                                onPressed: () {},
+                                onPressed: () {
+                                  _changeNum(_numList[0][2]);
+                                },
                                 child: Text(
-                                  _topList[2].toString(),
-                                  style: TextStyle(fontSize: 50),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Expanded(
-                      child: Row(
-                        children: <Widget>[
-                          Expanded(
-                            child: SizedBox(
-                              width: double.infinity,
-                              height: double.infinity,
-                              child: FlatButton(
-                                onPressed: () {},
-                                child: Text(
-                                  _cntList[0].toString(),
-                                  style: TextStyle(fontSize: 50),
-                                ),
-                              ),
-                            ),
-                          ),
-                          Expanded(
-                            child: SizedBox(
-                              width: double.infinity,
-                              height: double.infinity,
-                              child: FlatButton(
-                                onPressed: () {},
-                                child: Text(
-                                  _cntList[1].toString(),
-                                  style: TextStyle(fontSize: 50),
-                                ),
-                              ),
-                            ),
-                          ),
-                          Expanded(
-                            child: SizedBox(
-                              width: double.infinity,
-                              height: double.infinity,
-                              child: FlatButton(
-                                onPressed: () {},
-                                child: Text(
-                                  _cntList[2].toString(),
+                                  _numList[0][2],
                                   style: TextStyle(fontSize: 50),
                                 ),
                               ),
@@ -166,9 +181,11 @@ class _MyHomePageState extends State<MyHomePage> {
                               width: double.infinity,
                               height: double.infinity,
                               child: FlatButton(
-                                onPressed: () {},
+                                onPressed: () {
+                                  _changeNum(_numList[1][0]);
+                                },
                                 child: Text(
-                                  _btmList[0].toString(),
+                                  _numList[1][0],
                                   style: TextStyle(fontSize: 50),
                                 ),
                               ),
@@ -179,9 +196,11 @@ class _MyHomePageState extends State<MyHomePage> {
                               width: double.infinity,
                               height: double.infinity,
                               child: FlatButton(
-                                onPressed: () {},
+                                onPressed: () {
+                                  _changeNum(_numList[1][1]);
+                                },
                                 child: Text(
-                                  _btmList[1].toString(),
+                                  _numList[1][1],
                                   style: TextStyle(fontSize: 50),
                                 ),
                               ),
@@ -192,9 +211,62 @@ class _MyHomePageState extends State<MyHomePage> {
                               width: double.infinity,
                               height: double.infinity,
                               child: FlatButton(
-                                onPressed: () {},
+                                onPressed: () {
+                                  _changeNum(_numList[1][2]);
+                                },
                                 child: Text(
-                                  _btmList[2].toString(),
+                                  _numList[1][2],
+                                  style: TextStyle(fontSize: 50),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: SizedBox(
+                              width: double.infinity,
+                              height: double.infinity,
+                              child: FlatButton(
+                                onPressed: () {
+                                  _changeNum(_numList[2][0]);
+                                },
+                                child: Text(
+                                  _numList[2][0],
+                                  style: TextStyle(fontSize: 50),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: SizedBox(
+                              width: double.infinity,
+                              height: double.infinity,
+                              child: FlatButton(
+                                onPressed: () {
+                                  _changeNum(_numList[2][1]);
+                                },
+                                child: Text(
+                                  _numList[2][1],
+                                  style: TextStyle(fontSize: 50),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: SizedBox(
+                              width: double.infinity,
+                              height: double.infinity,
+                              child: FlatButton(
+                                onPressed: () {
+                                  _changeNum(_numList[2][2]);
+                                },
+                                child: Text(
+                                  _numList[2][2],
                                   style: TextStyle(fontSize: 50),
                                 ),
                               ),
